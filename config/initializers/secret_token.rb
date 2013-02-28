4719e9dc2eba4e12f9837e756da8bcac695cea08
jaime <jaime.victoria.90@gmail.com>
2013-02-28 15:59:06 -0600

    initialized OurStore App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OurStore::Application.config.secret_key_base = '7706c669c4b9fa5ed4e6bec4bbf0b4e0a6d777fa130856c456a28563ecfb37e2314b23bc5553bfdfa937694bd18ca8273671587d89bf11857f085a73cad5a6ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OurStore::Application.config.session_store :encrypted_cookie_store, key: '_OurStore_session'
